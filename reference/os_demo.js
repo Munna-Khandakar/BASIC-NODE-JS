a9174f1de56ac4e104e3e6d3eb125b34a887ff71
Munna-Khandakar <munnashisad@gmail.com>
2022-04-17 21:26:56 +0600

    os module

---
+++ reference/os_demo.js
@@ -0,0 +1,22 @@
+const os = require("os");
+
+// platofrm
+console.log(os.platform());
+
+// cpu arch
+console.log(os.arch());
+
+// cpu core info
+console.log(os.cpus());
+
+// free memory
+console.log(os.freemem());
+
+// total memory
+console.log(os.totalmem());
+
+// home dir
+console.log(os.homedir());
+
+// uptime
+console.log(os.uptime());
